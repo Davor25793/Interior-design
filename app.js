55b00162503829f4b5645c0fd28ac2cfcb7ebc6d
Davor25793 <roko257993@gmail.com>
2021-02-18 22:59:15 +0100

    changes

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ const hamburger = document.querySelector('.hamburger-menu')
 
 hamburger.addEventListener('click', () => {
   sidebar.classList.toggle('active')
+  
 })
 
 close.addEventListener('click', () => {
   sidebar.classList.toggle('active')
+})
+
+//Smooth scroll
+document.querySelectorAll('.nav-link').forEach(function(el){
+  el.addEventListener('click', function(e){
+    e.preventDefault()
+    const id = this.getAttribute('href')
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
 })
